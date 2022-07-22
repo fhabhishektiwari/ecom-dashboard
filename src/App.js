305d9511eedb75081cfdd00d66206e6692c0c50e
fhabhishektiwari <abhishektiwarifh@gmail.com>
2022-07-22 14:57:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='display-6 bg-danger text-light'>Ecom</h1>
+      <Button>submit</Button>
+    </div>
+  );
+}
+
+export default App;
